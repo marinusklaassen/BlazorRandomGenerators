8406b0f951898effeafdd983588cdac9ba7f5de2
Marinus Klaassen <marinus_klaassen@hotmail.com>
2020-12-29 14:18:04 +0100

    * TravestyInput.txt: * GlobalSuppressions.cs: * RandomGenerators.Travesty.csproj: * TravestyGeneratorTests.cs: * TravestyExpectedResult.txt: * RandomGenerators.Travesty.UnitTests.csproj:
    * TravestyGenerator.cs: Added a rough version of a travesty generator
      for processing text in a natural way.

---
+++ RandomGenerators.Travesty.UnitTests/TravestyGeneratorTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RandomGenerators.Travesty.UnitTests
+{
+    [TestClass]
+    public class TravestyGeneratorTests
+    {
+        private readonly string _fileDir = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+
+        [TestMethod]
+        public void ProcessText_MiddlingGarbling()
+        {
+            var sampleTextPath = _fileDir + "/TravestyInput.txt";
+            var sampleTextStreamReader = new StreamReader(sampleTextPath);
+            var sampleText = sampleTextStreamReader.ReadToEnd();
+
+            var travesty = new TravestyGenerator();
+            travesty.RandomInt = new Random(0);
+
+            var result = travesty.ProcessText(sampleText, 5);
+
+            var expectedResultPath = _fileDir + "/TravestyExpectedResult.txt";
+            var expectedResultStreamReader = new StreamReader(expectedResultPath);
+            var expectedResultText = expectedResultStreamReader.ReadToEnd();
+
+            Debug.WriteLine(expectedResultText);
+            Debug.WriteLine(result);
+
+            Assert.AreEqual(expectedResultText, result);
+        }
+    }
+}
---
+++ RandomGenerators.Travesty/TravestyGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace RandomGenerators.Travesty
+{
+    /// <summary>
+    /// The TravestyGenerator class contains methods to deal with natural language processing by randomization of input text. 
+    /// </summary>
+    public class TravestyGenerator
+    {
+        public Random RandomInt = new Random(Guid.NewGuid().GetHashCode());
+
+        /// <summary>
+        /// ProcessText transforms an input text into a pseudo text based on randomness and simple pattern matching.
+        ///
+        /// This method is loosely based on the Travesty program with in pascal this is described in the BYTE magazine issue of November 1984 VOL 9. NO.12.
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <param name="characterSequenceMatchOrder"></param>
+        /// <param name="maxLength"></param>
+        /// <returns>Processed text.</returns>
+        public string ProcessText(string inputText, int characterSequenceMatchOrder, int maxLength = 0)
+        {
+            if (!(characterSequenceMatchOrder > 0 && characterSequenceMatchOrder <= 9))
+                throw new ArgumentException(String.Format(
+                    "The method parameter '{0}' must be in the range of 1 and 9. The current value is {1}.",
+                    characterSequenceMatchOrder,
+                    nameof(characterSequenceMatchOrder)
+                    ));
+
+            if (inputText.Length < characterSequenceMatchOrder)
+                throw new ArgumentException(String.Format(
+                    "The method parameter '{0}' length cannot be smaller than the method parameter '{1}' length.",
+                    nameof(characterSequenceMatchOrder),
+                    nameof(inputText)
+                    ));
+
+            // If the maxLength is not set. Use inputText string length.
+            maxLength = maxLength > 0 ? maxLength : inputText.Length;
+
+            // Initial offset
+            var offsetMatchString = RandomInt.Next(maxLength);
+
+            var result = new StringBuilder();
+
+            // The actual processing of the input text.
+            while (result.Length < maxLength)
+            {
+                var matchString = GetSlice(inputText, characterSequenceMatchOrder, offsetMatchString);
+                var searchOffset = RandomInt.Next(maxLength);
+
+                for (int stringEnumerationIndex = 0; stringEnumerationIndex < inputText.Length; stringEnumerationIndex++)
+                {
+                    var searchIndex = (searchOffset + stringEnumerationIndex) % inputText.Length;
+                    var slice = GetSlice(inputText, characterSequenceMatchOrder, searchIndex);
+                    if (slice.Equals(matchString))
+                    {
+                        result.Append(slice);
+                        offsetMatchString = searchIndex + characterSequenceMatchOrder;
+                        break;
+                    }
+
+                }
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Helper method to get a wrapped slice from an input string.
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <param name="sliceLength"></param>
+        /// <param name="offset"></param>
+        /// <returns>A slice from an input string.</returns>
+        public string GetSlice(string inputText, int sliceLength, int offset)
+        {
+            var result = new StringBuilder();
+
+            for (int sliceIncrement = 0; sliceIncrement < sliceLength; sliceIncrement++)
+            {
+                var charSliceIndex = (offset + sliceIncrement) % inputText.Length;
+                result.Append(inputText[charSliceIndex]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
